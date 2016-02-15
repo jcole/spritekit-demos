4c8534995419ee8d89faaedd55ff1f8957f7a30e
Jeff Cole <jeffcole@jeffcole-macbookpro.local>
2016-02-15 13:57:01 -0500

    Initial Commit

---
+++ Jeff Game Demos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jeff Game Demos
+//
+//  Created by Jeff Cole on 2/15/16.
+//  Copyright © 2016 Jeff Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
